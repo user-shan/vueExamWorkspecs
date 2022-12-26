65c8ce09c2ec5667c65eb1bac7c7b235728143ff
sxy <m15046264025@163.com>
2022-12-26 19:01:13 +0800

    get

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+Vue.filter('dateformat', function (time) {
+    var dataStr = dayjs(time).format("YYYY-MM-DD HH:mm:ss");
+    return dataStr
+})
+// 全局过滤器，数字保留1位小数
+// Vue.filter('numfix1', num => parseFloat(num).toFixed(1))
+
 new Vue({
     el: "#appHome",
     data() {
@@ -34,8 +41,37 @@ new Vue({
                 repassword: [
                     { required: true, message: '请输入内容', trigger: 'blur' }
                 ]
-            }
+            },
             // 修改密码变量 end
+
+            // 添加的商品信息 start
+            addGood: { id: '', addname: '', addfort: '', addage: '' },
+            // 添加的商品信息 end
+
+            // 表格中商品信息 start
+            list: [
+                { id: 1, name: '蒙牛牛奶', age: 2, sex: '男', fort: "财务" },
+                { id: 2, name: '恰恰瓜子', age: 2, sex: '女', fort: "后勤" },
+                { id: 3, name: '恰恰瓜子', age: 2, sex: '女', fort: "生产" },
+                { id: 4, name: '恰恰瓜子', age: 2, sex: '男', fort: "餐饮" }
+            ],
+            // 表格中商品信息 end
+
+            // 下一个id
+            nextIda: 5,
+            // 每页可以显示的数据数量
+            pagesizes: [5, 10],
+            // 每页显示的具体的数据数量
+            pagesize: 5,
+            // 当前是第几页
+            currentPage: 1,
+            // 修改模块状态 true显示，false隐藏
+            dialogFormVisible: false,
+            // 修改的商品信息
+            editGood: { id: '', ediname: '', ediage: '', edisex: '', edifort: '' },
+            // 修改的id
+            editIda: 0,
+            formLabelWidth: '120px'
         }
     },
     methods: {
@@ -83,6 +119,75 @@ new Vue({
             this.$refs[formName].resetFields();
         },
         // 修改密码点击事件 end
+        // 添加商品
+        add() {
+            console.log("点击添加");
+            this.list.push(
+                {
+                    id: this.nextIda,
+                    name: this.addGood.addname,
+                    age: this.addGood.addage,
+                    sex: '男',
+                    fort: this.addGood.addfort
+                }
+            );
+            this.nextIda++;
+            // 页面输入框清空
+            this.addGood.addname = '';
+            this.addGood.addfort = '';
+            this.addGood.addage = '';
+        },
+        // 清空输入框
+        clearinput() {
+            this.addGood.addname = '';
+            this.addGood.addfort = '';
+            this.addGood.addage = '';
+        },
+        // 删除商品
+        del(id) {
+            this.list = this.list.filter((item) => item.id != id)
+        },
+        // handleSizeChange
+        handleSizeChange(val) {
+            this.pagesize = val;
+        },
+        handleCurrentChange(val) {
+            this.currentPage = val;
+        },
+        // 修改商品信息  row：当前行的数据，id:修改商品的序号
+        edit(row, id) {
+            // 记录当前修改商品的序号
+            this.editIda = id;
+
+            // 记录数据 
+            this.editGood.id = row.id;
+            this.editGood.ediname = row.name;
+            this.editGood.ediage = row.age;
+            this.editGood.edisex = row.sex;
+            this.editGood.edifort = row.fort;
+
+            // 显示窗口
+            this.dialogFormVisible = true;
+        },
+        // 点击确定修改数组中的商品信息
+        editDo() {
+            // 找到id对应数据的下标
+            var update_index = -1;
+            for (var i = 0; i < this.list.length; i++) {
+                if (this.list[i].id == this.editIda) {
+                    update_index = i;
+                    break;
+                }
+            }
+            // 修改update_index对应的商品信息
+            this.list[update_index].name = this.editGood.ediname;
+            this.list[update_index].age = this.editGood.ediage;
+            this.list[update_index].sex = this.editGood.edisex;
+            this.list[update_index].fort = this.editGood.edifort;
+
+            // 关闭dialog
+            this.dialogFormVisible = false;
+        }
     },
     created() {
         let that = this;
